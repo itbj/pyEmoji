a6ee4b385cf6c3ec8f45ededc499977bf252f83d
taoza <itbj00@163.com>
2022-09-18 18:01:16 +0800

    █
    █

--- a.py
+++ a.py
@@ -8,7 +8,7 @@ import colorama
 
 def progress_bar(progress, total,color=colorama.Fore.YELLOW):
     percent = 100 * ( progress / float(total))
-    bar = '.' * int (percent) + '-'*(100- int(percent))
+    bar = '█' * int (percent) + '-'*(100- int(percent))
     print (color + f"\r|{bar}| {percent:.2f}%", end="\r")
     if progress == total:
         print (colorama.Fore.GREEN + f"\r|{bar}| {percent:.2f}%", end="\r")
